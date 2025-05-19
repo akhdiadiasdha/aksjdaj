40606454537b5154902120136de12b7aa76b1f0d
akhdiadiasdha <mayer1122382+02@gmail.com>
2025-05-19 12:59:14 +0200

    lua

--- f.lua
+++ f.lua
@@ -15,7 +15,7 @@ better corner and more by vyxon
 
 --- FLUENT PLUS SETTINGS ---
 local Show_Button = true -- Shows the button for toggle fluent ui manually. If "false", works only on mobile, if "true", works everytime.
-local Button_Icon = "rbxassetid://135000736708602" -- Icon of the button for toggle fluent ui
+local Button_Icon = "rbxassetid://87077599107563" -- Icon of the button for toggle fluent ui
 ----------------------------
 
 local Lighting = game:GetService("Lighting")
